251248635883eac4b367bf06393d6b644bbbce8b
testehost <51829726+testehost@users.noreply.github.com>
2021-02-12 18:00:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,448 @@
+var pedidos = [
+    {
+        id: 1,
+        locker: 'Av Mutinga',
+        cliente: 'Jose',
+        pedido: '7639141',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,   
+        avaliacao: 'Ótimo',
+        pin: '458219',
+    },
+    {
+        id: 2,
+        locker: 'Shop Tiete',
+        cliente: 'Maria',
+        pedido: '7639142',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Retirado',
+        statusid: 2,       
+        avaliacao: 'Ótimo',
+        pin: '458220',
+    },
+    {
+        id: 3,
+        locker: 'Shop Tiete',
+        cliente: 'Renato',
+        pedido: '7639143',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Retirado',
+        statusid: 2,       
+        avaliacao: 'Ótimo',
+        pin: '458221',
+    },
+    {
+        id: 4,
+        locker: 'Shop Tiete',
+        cliente: 'Emerson',
+        pedido: '7639144',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,       
+        avaliacao: 'Ótimo',
+        pin: '458222',
+    },
+    {
+        id: 5,
+        locker: 'Shop Tiete',
+        cliente: 'Esdras',
+        pedido: '7639145',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Atrasado',
+        statusid: 3,       
+        avaliacao: 'Ótimo',
+        pin: '458223',
+    },
+    {
+        id: 6,
+        locker: 'Shop Tiete',
+        cliente: 'William',
+        pedido: '7639146',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,       
+        avaliacao: 'Ótimo',
+        pin: '458224',
+    },
+    {
+        id: 7,
+        locker: 'Av Mutinga',
+        cliente: 'Rosane',
+        pedido: '7639147',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,       
+        avaliacao: 'Ótimo',
+        pin: '458225',
+    },
+    {
+        id: 8,
+        locker: 'Shop Tiete',
+        cliente: 'Julio',
+        pedido: '7639148',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,       
+        avaliacao: 'Ótimo',
+        pin: '458225',
+    },
+    {
+        id: 9,
+        locker: 'Shop Tiete',
+        cliente: 'Diego',
+        pedido: '7639149',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,       
+        avaliacao: 'Ótimo',
+        pin: '458225',
+    },
+    {
+        id: 10,
+        locker: 'Shop Tiete',
+        cliente: 'Gustavo',
+        pedido: '7639150',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,       
+        avaliacao: 'Ótimo',
+        pin: '458225',
+    },
+    {
+        id: 11,
+        locker: 'Shop Tiete',
+        cliente: 'Emerson',
+        pedido: '7639144',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,       
+        avaliacao: 'Ótimo',
+        pin: '458222',
+    },
+    {
+        id: 12,
+        locker: 'Shop Tiete',
+        cliente: 'Esdras',
+        pedido: '7639145',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Atrasado',
+        statusid: 3,       
+        avaliacao: 'Ótimo',
+        pin: '458223',
+    },
+    {
+        id: 13,
+        locker: 'Shop Tiete',
+        cliente: 'William',
+        pedido: '7639146',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,       
+        avaliacao: 'Ótimo',
+        pin: '458224',
+    },
+    {
+        id: 14,
+        locker: 'Av Mutinga',
+        cliente: 'Rosane',
+        pedido: '7639147',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,       
+        avaliacao: 'Ótimo',
+        pin: '458225',
+    },
+    {
+        id: 15,
+        locker: 'Shop Tiete',
+        cliente: 'Julio',
+        pedido: '7639148',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,       
+        avaliacao: 'Ótimo',
+        pin: '458225',
+    },
+    {
+        id: 16,
+        locker: 'Shop Tiete',
+        cliente: 'Diego',
+        pedido: '7639149',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,       
+        avaliacao: 'Ótimo',
+        pin: '458225',
+    },
+    {
+        id: 10,
+        locker: 'Shop Tiete',
+        cliente: 'Gustavo',
+        pedido: '7639150',
+        email: 'email@newpost.com',
+        tel: '11 98452 4412', 
+        criado: '24/11/2020',
+        retirado: '25/11/2020',
+        tempomedio: '02:44:00',
+        status: 'Depositado',
+        statusid: 1,       
+        avaliacao: 'Ótimo',
+        pin: '458225',
+    },
+];
+
+    const tbody = document.querySelector('.tbody');
+    
+
+let switching = false;
+
+let reverso = false;
+
+let sortBtns = [...document.querySelectorAll('.sortBtn')]
+
+const propsHeader = ['locker', 'cliente', 'pedido', 'tel', 'criado', 'status']
+
+const props = Object.keys(pedidos[0])
+
+
+// preencher tabela
+// function preencherTabela(arr) {
+//     arr.forEach(pedido => {
+//         criarRow(pedido)
+//     })
+//     const paginas = Math.floor(pedidos.length / 7);
+//     console.log(paginas);
+// }
+
+
+
+document.querySelector('.headerMobileMenuBtn').addEventListener('click', e => {
+    e.target.firstElementChild.firstElementChild.classList.toggle('headerMobileSwitcher')
+    e.target.firstElementChild.lastElementChild.classList.toggle('headerMobileSwitcher')
+    document.querySelector('.headerMobile').classList.toggle('headerMobileVisible')
+})
+
+
+
+function criarRow(pedido) {
+
+    const tr = document.createElement('div')
+    tr.className = 'tr grid'
+    props.map((prop) => {
+        const td = document.createElement('div');
+        const span = document.createElement('span');
+
+        span.textContent = prop
+        if(prop === 'status') {
+            td.className = 'td' + ' ' + pedido.status;
+        } else {
+            td.className = 'td'
+        }
+        td.textContent = pedido[prop];
+        td.appendChild(span)
+        tr.appendChild(td)
+    })
+    const btn = document.createElement('div');
+    btn.classList = 'readMoreBtn'
+    tr.appendChild(btn);
+    tbody.appendChild(tr);
+}
+
+
+
+// preencherTabela(pedidos)
+
+let rows = [...document.querySelectorAll('.tr')]
+
+console.log(rows);
+
+
+
+
+// agrupar por propriedade
+function agrupar(arr, prop) {
+    const agrupado = arr.reduce((acum, pedido) => {
+        if(!acum[pedido[prop]]) {
+            acum[pedido[prop]] = []
+        }
+        acum[pedido[prop]].push(pedido)
+        return acum
+    }, {})
+    return agrupado
+}
+
+
+function classificar(arr, prop) {
+    let ordenado = [];
+    let agrupado = agrupar(arr, prop)
+    let tamanho = Object.keys(agrupado).length
+    for(let x in agrupado) {
+        agrupado[x].map(pedido => {
+            ordenado.push(pedido)
+        })
+    }
+    if(reverso == true) {
+        ordenado = ordenado.reverse();
+    }
+
+    for(pedido of ordenado) {
+        criarRow(pedido)
+    }
+        console.log(ordenado);
+};
+
+
+classificar(pedidos, 'locker')
+
+
+console.log(agrupar(pedidos, 'locker'));
+
+const table = document.querySelector('.table');
+
+
+table.addEventListener('click', e => {
+
+    if(e.target.classList.contains('sortBtn')) {
+        let prop = e.target.parentElement.textContent.trim().toLowerCase();
+        console.log(prop);
+        sortBtns.map(btn => {
+            btn.firstElementChild.removeAttribute("class");
+            btn.lastElementChild.removeAttribute("class");
+        })
+        if(switching) {
+            e.target.children[1].classList.add('sortToggle');
+            e.target.children[0].removeAttribute("class");
+            console.log(1);
+            switching = false
+            reverso = true
+            tbody.innerHTML = '';
+            classificar(pedidos, prop)
+        } else {
+            e.target.children[0].classList.add('sortToggle');
+            e.target.children[1].removeAttribute("class");
+            console.log(2);
+            switching = true
+            reverso = false
+            tbody.innerHTML = '';
+            classificar(pedidos, prop)
+        }
+    }
+
+    if(e.target.classList.contains('readMoreBtn')) {
+
+        console.log(e.target.parentElement);
+        e.target.classList.toggle('readMoreBtnOpen');
+        var nodes = e.target.parentElement.children;
+        console.log(nodes);
+        if(e.target.classList.contains('readMoreBtnOpen')) {
+            for(var i=0; i<nodes.length; i++) {
+                nodes[i].style.display = 'block';
+            }
+        } else {
+            let nods = [nodes[0], nodes[4], nodes[7], nodes[8], nodes[10], nodes[11], nodes[12]];
+            for(let x of nods) {
+                x.style.display = 'none';
+            }
+        }
+    }
+})
+
+sortBtns[0].children[1].classList.add('sortToggle');
+
+
+
+
+function preencherCards() {
+    let cards = Array.from(document.querySelectorAll('.card'));
+    let agrupados = agrupar(pedidos, 'status')
+    
+    cards[0].firstElementChild.textContent = ''
+    cards[1].firstElementChild.textContent = ''
+    cards[2].firstElementChild.textContent = ''
+    cards[3].firstElementChild.textContent = ''
+
+
+    cards[0].firstElementChild.textContent = (agrupados['Depositado'].length) + (agrupados['Atrasado'].length) + (agrupados['Retirado'].length)
+
+    function preencher(index, prop) {
+        if(agrupados[prop].length < 1) {
+            cards[index].firstElementChild.textContent = '0'
+        } else {
+            cards[index].firstElementChild.textContent = agrupados[prop].length
+        }
+    }
+
+    preencher(1, 'Depositado')
+    preencher(2, 'Atrasado')
+    preencher(3, 'Retirado')
+}
+
+preencherCards()
+
+
+
